d0394f56c0b5cb2ef93c495199cc831d52e151c8
george <ayellowman3@gmail.com>
2022-10-30 19:46:22 -0400

    add backend

--- init.py
+++ init.py
@@ -1,7 +1,11 @@
 import os
 os.system("pip install requests")
+os.system("pip install fastapi")
+os.system("pip install uvicorn")
 from startup import pull_sneakers
 pull_sneakers.main()
 
 
 #cd Documents\Coding\sneakers_startup
+os.system("docker build -t customerapi")
+os.system("docker run -p 8000:8000 customerapi")
