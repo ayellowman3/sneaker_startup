aa92cb1fac08a7fbb12312b05c61b488b4f36c3d
ayellowman3 <ayellowman3@gmail.com>
2022-11-04 03:52:55 -0400

    history page

--- frontend/app/client.py
+++ frontend/app/client.py
@@ -9,6 +9,8 @@ class requestHandler(BaseHTTPRequestHandler):
             do_homepage(self)
         elif self.path == '/cart':
             do_cart(self)
+        elif self.path == '/history':
+            do_history(self)
 
 def do_homepage(self):
     self.send_response(200)
@@ -18,9 +20,10 @@ def do_homepage(self):
     output = ''
     output += '<html><body>\n'
     output += '<a href="http://0.0.0.0:9000/cart">cart</a>'
+    output += '<a href="http://0.0.0.0:9000/history">cart</a>'
     output += '<div class="grid-container" style="display: grid; grid-gap: 20px; grid-template-columns: auto auto auto auto;">\n'
 
-    url = 'http://backend:8000/cart'
+    url = 'http://backend:8000/history'
     response = requests.get(url)
     data = json.loads(response.json())
     ids = data["ids"]
@@ -91,6 +94,39 @@ def do_cart(self):
         output += '}\n'
         output += '</script>\n'
 
+    output += '</body></html>'
+    self.wfile.write(output.encode())
+
+def do_history(self):
+    self.send_response(200)
+    self.send_header('content-type', 'text/html')
+    self.end_headers()
+
+    output = ''
+    output += '<html><body>\n'
+    output += '<p>Order History</p>'
+    output += '<a href="http://0.0.0.0:9000/">home</a>'
+    output += '<a href="http://0.0.0.0:9000/cart">cart</a>'
+    output += '<div class="grid-container" style="display: grid; grid-gap: 20px; grid-template-columns: auto auto auto auto;">\n'
+
+    url = 'http://backend:8000/history'
+    response = requests.get(url)
+    data = json.loads(response.json())
+    ids = data["ids"]
+
+    f = open(f'sneakers{os.sep}data{os.sep}sneakers.json')
+    data = json.load(f)
+
+    for sneaker in data['sneakers']:
+        if str(sneaker["id"]) in ids:
+            output += '<div class="grid-item" style="height: auto; width: 250px;" >\n'
+            output += f'<img alt="sneakers" src="{sneaker["imageUrl"]}" style="width: 204px; height: 145px;">\n'
+            output += f'<p>{sneaker["name"]}</p>\n'
+            output += '</div>\n'
+
+    f.close()
+    
+    output += '</div>\n'
     output += '</body></html>'
     self.wfile.write(output.encode())
 
